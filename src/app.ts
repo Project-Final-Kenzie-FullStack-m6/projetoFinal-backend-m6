ed8b6c277dd31f276f7aa298ef4304b266ffe818
Silas Junior <silas.mcoach@gmail.com>
2023-04-05 10:38:11 -0300

    feat: start base structure

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata"
+import cors from "cors"
+import express from "express"
+
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.use("/user")
+
+export default app;
