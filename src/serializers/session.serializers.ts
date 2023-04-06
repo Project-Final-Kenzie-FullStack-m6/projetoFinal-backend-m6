08bc54f0de8bb24b117333f4b15ca8ac40faba53
Rafael Soares <rafael.soaress012@gmail.com>
2023-04-06 10:12:54 -0300

    feat: login route created

--- src/serializers/session.serializers.ts
+++ src/serializers/session.serializers.ts
@@ -1,1 +1,8 @@
-//example
+import * as yup from "yup"
+
+
+
+export const userLoginSerializer= yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
