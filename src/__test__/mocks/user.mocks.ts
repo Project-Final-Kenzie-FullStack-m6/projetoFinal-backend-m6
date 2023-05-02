9c03f74d7436a87829c0a86622f566b84c5e75c0
= <danielcf.vix@hotmail.com>
2023-05-02 14:17:23 -0300

    fix: test folder

---
+++ src/__test__/mocks/user.mocks.ts
@@ -0,0 +1,60 @@
+export const mockedUserCreate = {
+    email: "rafaelS@gmail.com",
+    password: "123456",
+    name: "Rafael",
+    birthDate: "26/12/2001",
+    description:"vou apenas comprar carros",
+    isSeller: false,
+    address:{
+        cep:55660000,
+        state:"PE",
+        city:"Bezerros",
+        district:"Bairro A",
+        street:"rua 4",
+        number:13,
+        complement:"do lado de um pe de jambo"
+    }
+  };
+  
+  export const mockedUserNotInfoCreate = {
+    email: "",
+    password: "",
+    username: "",
+    name: "",
+  };
+  
+  export const mockedUpdateUserCreate = {
+    email: "updatetest00@mail.com",
+    password: "12345678",
+    name: "Rafael",
+    birthDate: "26/12/2001",
+    description:"vou apenas comprar carros",
+    isSeller: false,
+    address:{
+        cep:55660000,
+        state:"PE",
+        city:"Bezerros",
+        district:"Bairro A",
+        street:"rua 4",
+        number:13,
+        complement:"do lado de um pe de jambo"
+    }
+  };
+  
+  export const mockedSellerCreate = {
+    email: "rafael123@gmail.com",
+    password: "123456",
+    name: "Rafael",
+    birthDate: "26/12/2001",
+    description:"vou apenas comprar carros",
+    isSeller: true,
+    address:{
+        cep:55332000,
+        state:"PE",
+        city:"Bezerrosa",
+        district:"Bairro B",
+        street:"rua 5",
+        number:134,
+        complement:"do lado de um pe de jambo"
+    }
+  };
